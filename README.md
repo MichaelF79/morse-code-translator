82cfdf748271e2cdc9554f7153f4205aabb49b1e
Michael Ferreira <ferreira.mjm@gmail.com>
2021-03-30 15:49:27 +0100

    Styling added

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
+
 Morse Code Translator built using HTML, SCSS/CSS, and JavaScript.
+
 Github repository with regular commits and deployed to Github pages.
+
 Should be able to translate English to Morse code.
-** Extension -Should be able to translateMorse back to English
+
---
+++ main.js
@@ -0,0 +1,46 @@
+// have object with letters
+
+const morseObj = {
+	A: '.-',
+	B: '-...',
+	C: '-.-.',
+	D: '-..',
+	E: '.',
+	F: '..-.',
+	G: '--.',
+	H: '....',
+	I: '..',
+	J: '.---',
+	K: '-.-',
+	L: '.-..',
+	M: '--',
+	N: '-.',
+	O: '---',
+	P: '.--.',
+	Q: '--.-',
+	R: '.-.',
+	S: '...',
+	T: '-',
+	U: '..-',
+	V: '...-',
+	W: '.--',
+	X: '-..-',
+	Y: '-.--',
+	Z: '--..',
+	1: '.----',
+	2: '..---',
+	3: '...--',
+	4: '....-',
+	5: '.....',
+	6: '-....',
+	7: '--...',
+	8: '---..',
+	9: '----.',
+	0: '-----',
+};
+
+// pull typed text into JS
+
+const form = document.querySelector('form');
+const input = document.querySelector('input')
+const output = document.querySelector('output')
